0f6d289731d5f32bd1a6f338b9d07826df8b5f6c
Project 1.0 <163016882+proopz@users.noreply.github.com>
2024-03-19 22:32:28 +0530

    Update th.js

--- th.js
+++ th.js
@@ -2,8 +2,6 @@ const MY_KEY = process.env.MY_KEY;
 
 if (MY_KEY) {
   console.log("Successfully retrieved secret key:", MY_KEY);
-  document.write("<p>Successfully retrieved secret key: " + MY_KEY + "</p>");
 } else {
   console.error("Failed to retrieve secret key");
-  document.write("<p>Failed to retrieve secret key</p>");
 }
