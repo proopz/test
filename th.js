3a231f2bddbcb21b99a9ed4dfd7a942c0d27498a
Project 1.0 <163016882+proopz@users.noreply.github.com>
2024-03-23 00:02:32 +0530

    Update th.js

--- th.js
+++ th.js
@@ -1,1 +1,13 @@
-console.log("This is a sample JavaScript file.");
+// Validation function
+function validateData(data) {
+  const secret = window.MY_KEY; // Access the secret from window.MY_KEY
+  // Perform validation using the secret
+  if (data === secret) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Export the validateData function
+export { validateData };
